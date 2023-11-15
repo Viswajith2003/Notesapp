aa25cc5652939c03d714663c403ef53314374b58
Jerald <jeraldjoyson.inc@gmail.com>
2023-11-15 19:42:37 +0530

    complete the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     id("com.android.application")
     id("com.google.gms.google-services")
 }
-
 android {
     namespace = "com.example.notes_app"
     compileSdk = 33
@@ -35,7 +34,13 @@ dependencies {
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth-ktx:22.1.1")
+    implementation("com.google.firebase:firebase-auth:22.2.0")
+    implementation("com.google.firebase:firebase-firestore:24.9.1")
+    implementation("com.google.firebase:firebase-storage:20.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.google.firebase:firebase-analytics")
+    implementation ("com.firebaseui:firebase-ui-firestore:8.0.0")
+    implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
 }
--- app/src/main/java/com/example/notes_app/notesActivity.java
+++ app/src/main/java/com/example/notes_app/notesActivity.java
@@ -1,60 +1,84 @@
 package com.example.notes_app;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.Query;
 
 public class notesActivity extends AppCompatActivity {
 
     FloatingActionButton mcreatenotsfab;
+    RecyclerView recyclerView;
+    ImageButton menuBtn;
+    NoteAdapter noteAdapter;
     private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
-        mcreatenotsfab =findViewById(R.id.createnotefab);
+        mcreatenotsfab =findViewById(R.id.add_note_btn);
+        recyclerView = findViewById(R.id.recycler_view);
+        menuBtn = findViewById(R.id.menu_btn);
+
         firebaseAuth=FirebaseAuth.getInstance();
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setTitle("All Notes");
-        }
 
 
-        mcreatenotsfab.setOnClickListener(new View.OnClickListener() {
+        mcreatenotsfab.setOnClickListener((v)-> startActivity(new Intent(notesActivity.this,createnotes.class)));
+        menuBtn.setOnClickListener((v)->showMenu());
+        setupRecyclerView();
+    }
+
+    void showMenu(){
+        //Disply Menu
+        PopupMenu popupMenu = new PopupMenu(notesActivity.this,menuBtn);
+        popupMenu.getMenu().add("Logout");
+        popupMenu.show();
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(notesActivity.this,createnotes.class));
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                if (menuItem.getTitle()=="Logout"){
+                    FirebaseAuth.getInstance().signOut();
+                    startActivity(new Intent(notesActivity.this,MainActivity.class));
+                    finish();
+                    return true;
+                }
+                return false;
             }
         });
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu,menu);
-        return true;
+    void setupRecyclerView(){
+        Query query = Utility.getCollectionReferenceForNotes().orderBy("timestamp",Query.Direction.DESCENDING);
+        FirestoreRecyclerOptions<Note> option = new FirestoreRecyclerOptions.Builder<Note>()
+                .setQuery(query,Note.class).build();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        noteAdapter = new NoteAdapter(option,this);
+        recyclerView.setAdapter(noteAdapter);
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    protected void onStart() {
+        super.onStart();
+        noteAdapter.startListening();
+    }
 
-        int itemId = item.getItemId(); // Get the item ID once
+    @Override
+    protected void onStop() {
+        super.onStop();
+        noteAdapter.stopListening();
+    }
 
-        if (itemId == R.id.logout) {
-            firebaseAuth.signOut();
-            finish();
-            startActivity(new Intent(notesActivity.this, MainActivity.class)); // Assuming MainActivity is your login activity
-            return true;
-        }
-        return true;
+    @Override
+    protected void onResume() {
+        super.onResume();
+        noteAdapter.notifyDataSetChanged();
     }
 }
